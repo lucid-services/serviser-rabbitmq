cedad6df4f63d1da368e06e11800c5c8b9c6c6fa
fogine <7884288+fogine@users.noreply.github.com>
2017-11-09 18:56:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/BohemiaInteractive/bi-service-rabbitmq.svg?branch=master)](https://travis-ci.org/BohemiaInteractive/bi-service-rabbitmq)   
---
+++ index.js
@@ -0,0 +1,26 @@
+const service      = require('bi-service');
+const App          = require('./lib/app.js');
+const Router       = require('./lib/router.js');
+const Route        = require('./lib/route.js');
+const RouteType    = require('./lib/routeType.js');
+const Request      = require('./lib/request.js');
+const Response     = require('./lib/response.js');
+const configSchema = require('./lib/configSchema.js');
+
+service.AppManager.prototype.buildMQApp = buildMQApp;
+
+function buildMQApp(config, options) {
+    const app = new App(this, config, options);
+    this.add(app);
+    app.on('error', this.$buildAppErrorListener(app));
+    this.emit('build-app', app);
+    return app;
+}
+
+module.exports.App          = App;
+module.exports.Router       = Router;
+module.exports.Route        = Route;
+module.exports.RouteType    = RouteType;
+module.exports.Request      = Request;
+module.exports.Response     = Response;
+module.exports.configSchema = configSchema;

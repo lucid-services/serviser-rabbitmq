68da2a2ee393b3cc917669f11627c2e651416c84
fogine <7884288+fogine@users.noreply.github.com>
2017-11-10 19:22:16 +0100

    adds Service.prototype.buildMQApp syntax sugar method

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ function buildMQApp(config, options) {
     return app;
 }
 
+/**
+ * syntax sugar for building an {@link App} via {@link AppManager#buildMQApp}
+ *
+ * @public
+ * @param {String} name - one of the keys of `apps` service config section
+ * @param {Object} [options] - see {@link AppInterface} constructor options
+ * @return {App}
+ */
+Service.Service.prototype.buildMQApp = function(name, options) {
+
+    let defaults = {name: name};
+
+    let conf = this.config.getOrFail(`apps:${name}`);
+    conf = this.config.createLiteralProvider(conf);
+    options = _.assign(defaults, options);
+
+    return this.appManager.buildMQApp(conf, options);
+};
+
 module.exports.App          = App;
 module.exports.Router       = Router;
 module.exports.Route        = Route;
